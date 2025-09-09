61acee9ece7b2721834930c8b5d6d714f94c1f88
Matias Pedemonte <matiasepedemonte@gmail.com>
2025-09-09 01:04:38 -0300

    Commit productos

--- store-tpo/src/lib/utils.ts
+++ store-tpo/src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function fromObject<T extends object>(
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	cls: new (...args: any[]) => T,
+	obj: Partial<T>,
+): T {
+	const instance = new cls();
+	return Object.assign(instance, obj);
+}
